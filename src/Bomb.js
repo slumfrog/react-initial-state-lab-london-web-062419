a9bfe4dee2e9823bd10fa6648896f114779eb75c
slumfrog <kishan.anil@gmail.com>
2019-08-28 15:54:10 +0100

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,24 @@
-// your Bomb code here!
+import React, { Component } from "react";
+
+export default class Bomb extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      secondsLeft: props.initialCount
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.secondsLeft === 0 ? (
+          <p>Boom!</p>
+        ) : (
+          <p>{this.state.secondsLeft} seconds left before I go boom!</p>
+        )}
+      </div>
+    );
+  }
+}
+
+// alert (age >= 16) ? "You're allowed to drive!" : "You should be 16 to drive!"
